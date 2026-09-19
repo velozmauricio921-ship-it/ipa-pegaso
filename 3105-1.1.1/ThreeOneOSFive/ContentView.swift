e035ebf7d12595914481fe21ff2d66f43793c811
velozmauricio921-ship-it <velozmauricio921@gmail.com>
2026-09-18 20:21:19 -0400

    Update ContentView.swift

--- 3105-1.1.1/ThreeOneOSFive/ContentView.swift
+++ 3105-1.1.1/ThreeOneOSFive/ContentView.swift
@@ -350,9 +350,9 @@ private struct DashboardView: View {
 
     private var contactPanel: some View {
         VStack(spacing: 12) {
-            contactButton(title: "Discord", subtitle: "Join my Discord", icon: "bubble.left.fill", tint: accent, url: "https://discord.gg/aBQyPTbpgc")
+            contactButton(title: "Discord", subtitle: "Join my Discord Developer Server", icon: "bubble.left.fill", tint: accent, url: "https://discord.gg/aBQyPTbpgc")
             contactButton(title: "WhatsApp", subtitle: "Contact me", icon: "message.fill", tint: accent, url: "https://wa.me/584124788825")
-            contactButton(title: "VELOZxIOS", subtitle: "Developer", icon: "person.fill", tint: accent, url: "https://t.me/VELOZxIOS")
+            contactButton(title: "PEGASO", subtitle: "DUEÑO", icon: "person.fill", tint: accent, url: "https://t.me/desconocido_1412")
         }
     }
 
